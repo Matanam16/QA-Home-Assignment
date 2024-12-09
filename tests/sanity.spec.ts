621d2b7f47c75ba2cf211e7691b190f59e7c7113
Matanam16 <118213575+Matanam16@users.noreply.github.com>
2024-12-09 15:14:36 +0200

    Add files via upload

--- tests/sanity.spec.ts
+++ tests/sanity.spec.ts
@@ -1,10 +1,11 @@
+
+
 import { test, expect } from '@playwright/test';
 import BasePage from '../pages/BasePage';
 import HomePage from '../pages/HomePage';
 import LoginPage from '../pages/loginPage';
 import MenuPage from '../pages/MenuPage';
 import MyAcoountPage from '../pages/MyAccountPage';
-
 
 test.describe("Sanity Test", async () => {
   let basePage: BasePage;
@@ -17,10 +18,9 @@ test.describe("Sanity Test", async () => {
   const password = '123456';
   const login = 'Log in';
   const myProfile = 'MY PROFILE';
-  const nickName = 'Testing';
+  const nickName = '';
   const apply = 'Apply';
   const balance = ['318,992,450' , '26,922.20']
-
 
   test.beforeEach(async ({page}) => {
      basePage = new BasePage(page);
@@ -39,14 +39,16 @@ test.describe("Sanity Test", async () => {
     await test.step("Navigate to System And Login", async () => {
       await basePage.GoToUrl(url);
       await homePage.clickOnLoginBtn();
-      await loginPage.loginToTheSystem(email , password, login);
+      await loginPage.loginToTheSystem(email , password);
     });
 
     await test.step("Go To The Menu, Change The Username And The Avatar", async () => {
       await homePage.openMenu();
       await menuPage.clickOnMyAccount();
       await myAcoountPage.openMyProfile(myProfile);
-      await myAcoountPage.selectNickname(nickName);
+      await myAcoountPage.getNickname(nickName);
+      await myAcoountPage.clickOnEdit();
+      await myAcoountPage.fillRandomNickname(nickName);
       await myAcoountPage.selectRandomAvatr();
       await myAcoountPage.clickApply(apply);
       await myAcoountPage.validateThatTheNiicknameChanged(myProfile, nickName);
@@ -61,6 +63,5 @@ await test.step("Print the user's coin amount for both coin types", async () => 
  
 });
 
-
   });
-});
+}); 
